b06600ecdf6efd4e1567bc50f079ff4e06b87be6
Aaron NA <hna206@naver.com>
2021-07-15 01:32:54 +1200

    Add project files.

---
+++ AbstractFactory/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public interface Animal
+    {
+        string speak();
+    }
+
+    public class Cat : Animal
+    {
+        public string speak()
+        {
+            return "Meow Meow Meow";
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public string speak()
+        {
+            return "Roar";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string speak()
+        {
+            return "Bark bark";
+        }
+    }
+
+    public class Octopus : Animal
+    {
+        public string speak()
+        {
+            return "SQUAWCK";
+        }
+    }
+    public class Shark : Animal
+    {
+        public string speak()
+        {
+            return "Cannot Speak";
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = null;
+            AnimalFactory animalFactory = null;
+            string speakSound = null;
+
+            animalFactory = AnimalFactory.CreateAnimalFactory("Sea");
+            Console.WriteLine("Animal Factory type: " + animalFactory.GetType().Name);
+            Console.WriteLine();
+
+            Console.WriteLine("-------------------------");
+            animalFactory = AnimalFactory.CreateAnimalFactory("Land");
+            Console.WriteLine("Animal Type : " + animal.GetType().Name);
+            speakSound = animal.speak();
+            Console.WriteLine(animal.GetType().Name + " Speak : " + speakSound);
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
